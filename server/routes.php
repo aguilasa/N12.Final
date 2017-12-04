56f1a0e0128982266b0e32159122cc8081538aef
aguilasa <ingmar.aguiar@gmail.com>
2017-12-04 18:19:31 -0200

    Main

--- server/routes.php
+++ server/routes.php
@@ -3,7 +3,5 @@
 $app->group('/perguntas', function () {
     $this->get('', '\App\Controllers\PerguntasController:listAll');
     $this->post('', '\App\Controllers\PerguntasController:create');
-    $this->get('/{id:[0-9]+}', '\App\Controllers\PerguntasController:view');
-    $this->put('/{id:[0-9]+}', '\App\Controllers\PerguntasController:update');
-    $this->delete('/{id:[0-9]+}', '\App\Controllers\PerguntasController:delete');
+    $this->get('/resultados', '\App\Controllers\PerguntasController:results');
 });
